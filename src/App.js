70218fd274bf690a6e7150e34112f1958b47c49d
NinePiece2 <romit.sagu@hotmail.com>
2024-06-05 01:19:01 -0400

    Small Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { setProjects } from './features/portfolio/portfolioSlice';
 import ProjectList from './components/ProjectList';
 import Header from './components/Header';
