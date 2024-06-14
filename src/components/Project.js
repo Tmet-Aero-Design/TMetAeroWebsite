5107c79dbfdfd02b3d8bf8808394abc7d3434cc4
NinePiece2 <romit.sagu@hotmail.com>
2024-06-13 20:40:33 -0400

    Fixing Accent Colours

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,7 @@ const Project = ({ project }) => {
       </div>
       <h2 className="text-xl font-medium title-font text-gray-900 mt-5">{project.title}</h2>
       <p className="text-base leading-relaxed mt-2">{project.description}</p>
-      <a className="text-indigo-500 inline-flex items-center mt-3" href={project.link}>
+      <a className="text-blue-500 inline-flex items-center mt-3" href={project.link}>
         Learn More
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
