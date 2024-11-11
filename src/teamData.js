79ba981efd2b1bee30cf881aac0e81c97ca54d49
NinePiece2 <romit.sagu@hotmail.com>
2024-11-10 23:51:14 -0500

    Change Marketing to  Media & Marketing

--- src/teamData.js
+++ src/teamData.js
@@ -359,7 +359,7 @@ const teamMembers = [
     {
       name: "Jadon Jackman",
       role: "Marketing Director",
-      subteam: "Marketing",
+      subteam: "Media & Marketing",
       description: "Hi my name is Jadon. I'm in my second year of aerospace engineering and I am this year's marketing director. When I'm not studying I'm either drawing or in the gym. If you ever see me around campus feel free to stop me.",
       image: "team_pictures/JadonJ.jpg",
       social: {
@@ -372,7 +372,7 @@ const teamMembers = [
     {
       name: "Sharon Ebenesar",
       role: "Graphic Designer",
-      subteam: "Marketing",
+      subteam: "Media & Marketing",
       description: "Hello!! I'm going into my third year of Aerospace Engineering and I'm going to be the graphics designer for the team.",
       image: "team_pictures/Sharon-Ebenesar.png",
       social: {
@@ -385,7 +385,7 @@ const teamMembers = [
     {
       name: "Arshad Usman",
       role: "Web Developer",
-      subteam: "Marketing",
+      subteam: "Media & Marketing",
       description: "Software Engineer suffering from a major case of senioritis.",
       image: "team_pictures/Arshad-Usman.jpeg",
       social: {
