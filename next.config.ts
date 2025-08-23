a71472523cfb2aa85c9eee4dff872247c83c9a7d
Romit Sagu <romit.sagu@hotmail.com>
2025-08-23 14:38:01 -0400

    Try to fix deploy issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/TMetAeroWebsite" : "",
+  assetPrefix: isProd ? "/TMetAeroWebsite/" : "",
 };
 
 export default nextConfig;
